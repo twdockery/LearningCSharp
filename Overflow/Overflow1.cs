120bdb4bdb69d57059639d6277545c337e78a349
twdockery <62146335+twdockery@users.noreply.github.com>
2020-04-08 17:51:08 -0400

    Understanding overflow
    building byte and overflowing values

--- Overflow/Overflow1.cs
+++ Overflow/Overflow1.cs
@@ -4,9 +4,11 @@ namespace HelloWorld
 {
     class Program
     {
+        byte number = 255;
+number = number +1 // 0 because of overflow
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("255 + 1 =" number);
         }
     }
 }
